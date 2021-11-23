4040abf5947d0d538769b8a97f123a270ca7ac6e
ChinmayNaik27 <94610060+ChinmayNaik27@users.noreply.github.com>
2021-11-23 08:04:59 +0530

    For_Loop_Programs

---
+++ For_Loop/Print_Hey_5_times.py
@@ -0,0 +1,3 @@
+#WAP using for loop print hello 5 times
+for i in range(0,5):
+    print("Hello")
---
+++ For_Loop/Structure_3.py
@@ -0,0 +1,11 @@
+"""WAP TO PRINT THE GIVEN OUTPUT:
+*****
+****
+***
+**
+*
+"""
+for i in range(0,5):
+    for j in range(i,5):
+        print("*",end="")
+    print("")
