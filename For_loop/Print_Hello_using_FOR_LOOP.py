fabce6c2e118aef306d92bbaf0d25aafd65c7749
ChinmayNaik27 <94610060+ChinmayNaik27@users.noreply.github.com>
2021-11-23 08:03:31 +0530

    Delete For_loop directory

--- For_loop/Print_Hello_using_FOR_LOOP.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-str1="Hello Buddy"
-for i in str1:
-=======
-str1="Hello Buddy"
-for i in str1:
->>>>>>> f4fd73546fac18b01d19aa9c7b62c40757dba13a
-    print(i)
--- For_loop/Print_Hey_5_times.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-#WAP using for loop print hello 5 times
-for i in range(0,5):
-=======
-#WAP using for loop print hello 5 times
-for i in range(0,5):
->>>>>>> f4fd73546fac18b01d19aa9c7b62c40757dba13a
-    print("Hello")
