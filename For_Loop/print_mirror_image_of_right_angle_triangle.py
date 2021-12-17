524861849b3da12bd1af68c096074a32f4acbaee
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2021-12-17 19:02:29 +0530

    For_Loop

---
+++ For_Loop/print_mirror_image_of_right_angle_triangle.py
@@ -0,0 +1,17 @@
+"""WAP to print
+    *
+   **
+  ***
+ ****
+*****"""
+n=3
+for i in range (1,5):
+    k=0
+    while k < n:
+        print(" ",end="")
+        k+=1
+        n-=1
+    for j in range(1,i+1):
+        print("*",end=" ")
+
+    print("")
