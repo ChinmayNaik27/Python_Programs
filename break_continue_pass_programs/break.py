dcbd3e744b9529b1c2b8dc907ce5b59b8bb132fd
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2021-12-17 19:16:12 +0530

    Contuine_and_break_examples

---
+++ break_continue_pass_programs/break.py
@@ -0,0 +1,9 @@
+##WAP for break
+a= int(input("Enter number of repitation to be print:"))
+i=0
+while i<=a:
+    if i==5:
+        print("There is a break")
+        break
+    i+=1
+    print(i)
---
+++ break_continue_pass_programs/continue.py
@@ -0,0 +1,7 @@
+#WAP To Print a and b values
+
+for a in range (1,5):
+    for j in range (1,5):
+        if a==3:
+            continue
+        print(a,j)
