8386bbddccdceb7fcb4f6aeaa1661fa4e1c8627f
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2021-12-19 13:56:03 +0530

    Program

--- list/Zero_base_index_program.py
+++ list/Zero_base_index_program.py
@@ -1,4 +1,4 @@
-##accessing list by zero base indexing
+#accessing list by zero base indexing
 
 list1=[10,20,[10,20,30],'abc',30,40]
 print(list1)                                  #print complete list
