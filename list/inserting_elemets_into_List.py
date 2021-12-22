012bee7fca6633365b53acc118422e9324e6163a
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2021-12-22 16:07:13 +0530

    List_Programs

---
+++ list/inserting_elemets_into_List.py
@@ -0,0 +1,14 @@
+#WAP To Insert Elements into the List
+a1=input("Enter 1st item:")
+a2=input("Enter 2nd item:")
+a3=input("Enter 3rd item:")
+a4=input("Enter 4th item:")
+a5=input("Enter 5th item:")
+a6=input("Enter 6th item:")
+a7=input("Enter 7th item:")
+a8=input("Enter 8th item:")
+a9=input("Enter 9th item:")
+list1=[a1,a2,a3,a4,a5,a6,a7,a8,a9]
+print("All the items in list are :\n",)
+for a in list1:
+    print(a)
