d1b92598c1d521f302352b5676b9a26aee443ad6
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-01-01 11:07:12 +0530

    Tuples_Programs

---
+++ Tuples/Basics.py
@@ -0,0 +1,14 @@
+#Basics of Tuple
+tuple1=(1,2,3,'abc',1,1.2,56)
+print(tuple1)
+x=tuple1.index(2)
+print(x)
+y=tuple1.count(1)
+print(y)
+#accessing tuple is same as list
+print(tuple1[2:5])
+print(tuple1[:5])
+print(tuple1[2:])
+print(tuple1[::])
+print(tuple1[::-1])
+print(tuple1[:-1])
