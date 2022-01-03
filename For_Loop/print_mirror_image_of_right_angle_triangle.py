3c6d3f0d7c484673f92d7fab9e0fb0d991af3e51
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-01-03 13:49:34 +0530

    For_Loop_Programs

--- For_Loop/print_mirror_image_of_right_angle_triangle.py
+++ For_Loop/print_mirror_image_of_right_angle_triangle.py
@@ -4,14 +4,13 @@
   ***
  ****
 *****"""
-n=5
-for i in range (6):
+n=3
+for i in range (1,5):
     k=0
     while k<n:
         print(" ",end="")
-        n-=1
         k+=1
-    for j in range(0,i):
-        print("*",end=" ")
-
+    n-=1
+    for j in range(1,i+1):
+        print("*",end="")
     print("")
