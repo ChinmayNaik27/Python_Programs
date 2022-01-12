173bc871c9b3a1b6f45ad36570dcc2608f8a70a7
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-01-12 09:34:49 +0530

    String-Programs

---
+++ StringHandling/string_types.py
@@ -0,0 +1,14 @@
+"""types of string"""
+s1="This is a String"
+s2='This is a string'
+s3="""
+    This is Line1
+    this is line 2
+    this is line 3"""
+print(type(s1))
+print(s1)
+print(type(s2))
+print(s2)
+print(type(s3))
+print(s3)
+print(s3.strip())
