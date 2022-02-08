c994153fa2c2ad3b6333a295eb097aa8ab8d58e8
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-02-08 16:16:38 +0530

    Modules

--- Modules/importing_module_on_different_location.py
+++ Modules/importing_module_on_different_location.py
@@ -1,7 +1,7 @@
 #importing module present on different location
 #this is adding path manually
 import sys
-sys.path("C:\\Users\\chinm/OneDrive/Desktop/mymodule1.py")
+sys.path("C:/Users/chinm/OneDrive/Desktop/mymodule1.py")
 #importing module
 import mymodule1
 a=mymodule1.add(10,20)
